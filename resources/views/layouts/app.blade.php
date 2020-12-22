fba4352fb8ec593eb2c64e0a5784578ec7dfd092
h.k <hkaneyama1@outlook.jp>
2020-12-22 14:25:53 +0000

    @incloud navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,29 +8,8 @@
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">Tasklist</a>
-
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        {{-- ユーザ登録ページへのリンク --}}
-                        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                        {{-- ログインページへのリンク --}}
-                        <li><a href="#">Login</a></li>
-                        
-                        {{-- タスク作成ページへのリンク --}}
-                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
 
         <div class="container">
             {{-- エラーメッセージ --}}
